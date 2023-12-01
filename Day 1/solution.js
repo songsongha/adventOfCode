69e65a653b33fa07da89b7a0868682fa6554d8b8
Song Ha <songsongha@gmail.com>
2023-12-01 15:17:53 -0700

    D1 P2 Solution

--- Day 1/solution.js
+++ Day 1/solution.js
@@ -5,17 +5,43 @@ const fs = require('fs')
 // put inputs from text file into an array
 const puzzleData = fs.readFileSync('./input.txt', 'utf8').split('\n')
 
-const numberArray = puzzleData.map((line)=>{ 
-    const digitsOnly = line.match(/\d/g)
-    if (digitsOnly?.length){
-        // return first and last digits or double up if there's only 1 digit
-        return Number (digitsOnly[0] + digitsOnly[digitsOnly.length-1])
+const findSumOfFirstAndLastDigits = (puzzleData) => {
+
+    const numberArray = puzzleData.map((line)=>{ 
+        const digitsOnly = line.match(/\d/g)
+        if (digitsOnly?.length){
+            // return first and last digits or double up if there's only 1 digit
+            return Number (digitsOnly[0] + digitsOnly[digitsOnly.length-1])
+        }
+        else {
+            return Number(0)
+        }
+    })
+
+    const sum = numberArray.reduce((acc, curr) => acc + curr, 0)
+    return sum
+}
+
+console.log(findSumOfFirstAndLastDigits(puzzleData)) // 55130
+
+// some entries have overlapping letters i.e. xtwone3four numbers so the we need to put the letters back in so ensure all numbers are placed
+const wordToNumberMap = {
+    one: 'one1one',
+    two: 'two2two',
+    three: 'three3three',
+    four: 'four4four',
+    five: 'five5five',
+    six: 'six6six',
+    seven: 'seven7seven',
+    eight: 'eight8eight',
+    nine: 'nine9nine'
+}
+const wordsReplacedWithNumberArray = puzzleData.map((line)=>{
+    let newLine = line
+    for (const key in wordToNumberMap){
+        newLine = newLine.replaceAll(key,wordToNumberMap[key])
     }
-    else {
-        return Number(0)
-    }
+    return newLine
 })
 
-const sum = numberArray.reduce((acc, curr) => acc + curr, 0)
-
-console.log({sum})
+console.log (findSumOfFirstAndLastDigits(wordsReplacedWithNumberArray)) // 54985
