4c93102be05a00ef8cabf095d398163a9297c933
Song Ha <songsongha@gmail.com>
2023-12-03 08:53:44 -0700

    WIP Day 1 Problem 1

---
+++ 2023(Javascript)/Day 2/solution.js
@@ -0,0 +1,29 @@
+// Problem 1: Find games that are valid with 12 red cubes, 13 green cubes, and 14 blue cubes and sum the game ids
+
+const fs = require('fs')
+
+// put inputs from text file into an array
+const puzzleData = fs.readFileSync('./input.txt', 'utf8').split('\n')
+const regex = /(\d+) (red|blue|green|:)/g
+let sum = 0
+
+puzzleData.forEach((game, gameIndex)=>{
+    const match = game.match(regex)
+    console.log({match})
+    const isValid = match.every((result)=>{
+        const splitResult = result.split(' ')
+        if (splitResult[1].includes('red') && Number(splitResult[0]) >= 12 
+        || splitResult[1].includes('green') && Number(splitResult[0]) >= 13 
+        || splitResult[1].includes('red') && Number(splitResult[0]) >= 14) {
+            console.log('invalid game', gameIndex + 1)
+            return false
+        }
+        return true
+        
+    })
+    if (isValid) {
+        const gameId = gameIndex + 1
+        sum += gameId
+    }
+})
+console.log({sum})
