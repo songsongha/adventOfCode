0fe91ecbcaaa1a9f14aafc9fd1ddf97decaa1ccf
Song Ha <songsongha@gmail.com>
2023-12-03 21:43:18 -0700

    Day 2 Problem 2 Solution

--- 2023(Javascript)/Day 2/solution.js
+++ 2023(Javascript)/Day 2/solution.js
@@ -1,4 +1,5 @@
 // Problem 1: Find games that are valid with 12 red cubes, 13 green cubes, and 14 blue cubes and sum the game ids
+// Problem 2: Find minimum  number of cubes for each game, multiply the number of die for each game then add them together.
 
 const fs = require('fs')
 
@@ -14,7 +15,6 @@ puzzleData.forEach((game, gameIndex)=>{
         if (splitResult[1].includes('red') && Number(splitResult[0]) > 12 
         || splitResult[1].includes('green') && Number(splitResult[0]) > 13 
         || splitResult[1].includes('blue') && Number(splitResult[0]) > 14) {
-            console.log('invalid game', gameIndex + 1)
             return false
         }
         return true
@@ -26,3 +26,25 @@ puzzleData.forEach((game, gameIndex)=>{
     }
 })
 console.log({sum})
+
+let powerSum = 0
+
+puzzleData.forEach((game)=>{
+    let minRed = 0
+    let minGreen = 0
+    let minBlue = 0
+
+    const match = game.match(regex)
+    match.forEach((result)=>{
+        const splitResult = result.split(' ')
+        const diceCount = Number(splitResult[0])
+        if (splitResult[1].includes('red') && diceCount > minRed) minRed = diceCount
+        if (splitResult[1].includes('green') && diceCount > minGreen) minGreen = diceCount
+        if (splitResult[1].includes('blue') && diceCount > minBlue) minBlue = diceCount
+    })
+
+    const power = minRed * minGreen * minBlue
+    powerSum += power
+})
+
+console.log({powerSum})
