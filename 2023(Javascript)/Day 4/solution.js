bd485e76e7ed5b3c69c8c0b7e03a15eda090d4de
Song Ha <songsongha@gmail.com>
2023-12-05 21:57:53 -0700

    Day 4 P1 Solution

--- 2023(Javascript)/Day 4/solution.js
+++ 2023(Javascript)/Day 4/solution.js
@@ -1,4 +1,5 @@
-// Problem 1: Find values of numbers that are adjacent/diagonal to a symbol
+// Problem 1: Find number of points based on winning numbers
+// Problem 2: How many scratch cards to do you have if you get a copy for every winning number?
 
 const fs = require('fs')
 
@@ -6,17 +7,35 @@ const fs = require('fs')
 const puzzleData = fs.readFileSync('./input.txt', 'utf8').split('\n')
 
 let sum = 0
+const arrayOfWins = []
+
 puzzleData.forEach((line)=>{
     const numbers = line.split(': ').pop().split(' | ')
     const winningNumbers = numbers[0].match(/\d+/g)
     const ticketNumbers = numbers[1].match(/\d+/g)
     const intersection = winningNumbers.filter(position => ticketNumbers.includes(position))
-    
+    arrayOfWins.push(intersection)
     if (intersection.length){
         const points = 2**(intersection.length - 1)
         sum += points
     } 
 })
 
+const cardCount = new Array (arrayOfWins.length).fill(1)
+
+arrayOfWins.forEach((intersection, index) =>{
+    for (let j = 0; j < cardCount[index]; j++){
+        for (let i = 0; i < intersection.length; i++){
+            cardCount[index+i+1] +=1
+        }
+    }
+})
+const totalNumCards = cardCount.reduce((acc, curr) => acc + curr, 0)
+
 console.log({sum})
+console.log({totalNumCards})
+
+
+
+
 
