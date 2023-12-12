f5082598a5b0a573b8393e313e506417a2d28ddb
Song Ha <songsongha@gmail.com>
2023-12-12 00:55:23 -0700

    Day 5 Problem 2 Solution brute force method

--- 2023(Javascript)/Day 5/solution.js
+++ 2023(Javascript)/Day 5/solution.js
@@ -30,7 +30,6 @@ mapArray[0][0].forEach(seed => {
     // find corresponding soil -> location
     let refSource = seed
     for(let i = 1; i < mapArray.length; i++){
-        console.log({refSource})
         for(let j = 0; j < mapArray[i].length; j++){
             const source = mapArray[i][j][1]
             const destination = mapArray[i][j][0]
@@ -53,3 +52,36 @@ mapArray[0][0].forEach(seed => {
 
 console.log({locationArray})
 console.log('Minimum value', Math.min(...locationArray))
+
+// Problem 2 Brute Force method
+
+let minLocation
+// for each seed
+for(let k = 0; k < mapArray[0][0].length -1; k = k+2){
+    const seedStart = mapArray[0][0][k]
+    const seedRange = mapArray[0][0][k+1]
+    for(let seed = seedStart; seed <= seedStart + seedRange; seed++){
+        let refSource = seed
+        for(let i = 1; i < mapArray.length; i++){
+            for(let j = 0; j < mapArray[i].length; j++){
+                const source = mapArray[i][j][1]
+                const destination = mapArray[i][j][0]
+                const range = mapArray[i][j][2]
+                if (refSource >= source && refSource <= source + range) {
+                    // refSource falls in the range
+                    refSource = destination + refSource - source
+                    break
+                } // otherwise refSource should remain the same
+            }
+    
+            // if we are on location
+            if (i === mapArray.length -1) {
+                if (!minLocation || minLocation > refSource){
+                    minLocation = refSource
+                }
+            }
+            
+        }
+    }
+}
+console.log({minLocation})
