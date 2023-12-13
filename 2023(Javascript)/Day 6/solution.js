0a97fe1a0509ac02979ec6865842410339d39a9a
Song Ha <songsongha@gmail.com>
2023-12-12 21:39:04 -0700

    Day 6 P2 Solution

--- 2023(Javascript)/Day 6/solution.js
+++ 2023(Javascript)/Day 6/solution.js
@@ -1,4 +1,4 @@
-// Problem 1: What is the lowest location number that corresponds to any of the initial seed numbers
+// Problem 1: Multiply number of ways to win for given race data
 
 const fs = require('fs')
 
@@ -16,6 +16,7 @@ const raceDuration = raceData[0]
 const recordDistance = raceData[1]
 
 const waysToWinArray = []
+
 raceDuration.forEach((duration, index) =>{
     let waysToWin = 0
     for (let i = 1; i <= duration; i++) {
@@ -27,3 +28,19 @@ raceDuration.forEach((duration, index) =>{
 
 const p1Solution = waysToWinArray.reduce((acc, curr) => acc * curr, 1)
 console.log({p1Solution})
+
+// Problem 2: Number of ways to win if spaces are removed from data?
+const raceData2 = puzzleData.map (line =>{
+    return Number(line.match(regex).reduce((acc, curr) => acc + curr))
+    })
+
+const raceDuration2 = raceData2[0]
+const recordDistance2 = raceData2[1]
+
+let waysToWin = 0
+    for (let i = 1; i <= raceDuration2; i++) {
+        const distanceTravelled = i * (raceDuration2 - i)
+        if (distanceTravelled > recordDistance2) waysToWin++
+    }
+
+console.log({waysToWin})
