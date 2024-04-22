f5aa98083c5ef0220f1a4941d62583e397da6ccc
songsongha <songsongha@gmail.com>
2024-04-22 04:25:14 +0000

    Day 9 Part 2 Solution

--- 2023(Javascript)/Day 9/solution.js
+++ 2023(Javascript)/Day 9/solution.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 // put inputs from text file into an array
 const puzzleData = fs.readFileSync('./input.txt', 'utf8').split('\n')
 
-const calculateExtrapolatedValue = (stringOfNumbers) => {
+const calculateHistory = (stringOfNumbers)=>{
     const arrayOfNumbers = stringOfNumbers.split(' ').map(stringNum=>Number(stringNum))
     const historyArray = [arrayOfNumbers]
     let history = [...arrayOfNumbers]
@@ -18,19 +18,38 @@ const calculateExtrapolatedValue = (stringOfNumbers) => {
         historyArray.push(nextHistory)
         history = [...nextHistory]
     }
-
+    return historyArray
+}
+const calculateExtrapolatedValue = (stringOfNumbers) => {
+    const historyArray = calculateHistory(stringOfNumbers)
     let extrapolatedValue = 0
     for (let i = historyArray.length -1; i >= 0; i--){
         extrapolatedValue += historyArray[i].pop()
     }
     return extrapolatedValue
+}
+
+const calculateExtrapolatedValue2 = (stringOfNumbers) => {
+    const historyArray = calculateHistory(stringOfNumbers)
+    let extrapolatedValue = 0
+    for (let i = historyArray.length -1; i >= 0; i--){
+        extrapolatedValue = historyArray[i].shift() - extrapolatedValue
+    }
+    return extrapolatedValue
 
 }
 
-let sumOfExtrapolatedValues = 0
+let sumOfExtrapolatedValuesPart1 = 0
 puzzleData.forEach(line => {
-    sumOfExtrapolatedValues += calculateExtrapolatedValue(line)
+    sumOfExtrapolatedValuesPart1 += calculateExtrapolatedValue(line)
+})
+let sumOfExtrapolatedValuesPart2 = 0
+puzzleData.forEach(line => {
+    sumOfExtrapolatedValuesPart2 += calculateExtrapolatedValue2(line)
 })
 
-console.log({sumOfExtrapolatedValues})
+console.log({sumOfExtrapolatedValuesPart1})
+console.log({sumOfExtrapolatedValuesPart2})
+
+
 
