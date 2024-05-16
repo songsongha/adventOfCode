52f6ee73bcada42ce2a2f85c452c8f42749773c7
Song <songsongha@gmail.com>
2024-05-16 09:30:51 -0600

    Day 12 - Add brute force method

--- 2023(Javascript)/Day 12/solution.js
+++ 2023(Javascript)/Day 12/solution.js
@@ -4,45 +4,45 @@ const fs = require('fs')
 
 const countSpringArrangements = (conditionReport, damageArray, memo = {}) => {
     console.log({conditionReport})
+    let totalCount = 0
     // condition report is a string
     // damageArray is an array of numbers
     // return a number
-// split condtionreport at first .
-// const groupings = conditionReport.split('.').filter(item=>!!item)
-// console.log({groupings})
 if (conditionReport.includes('?')) {
     // if the conditionReport has unknowns, create all the possible branches
     const springOptions = ['.', '#']
     for (let option of springOptions) {
         const newConditionReport = conditionReport.replace('?', option)
         console.log({newConditionReport})
-        countSpringArrangements(newConditionReport, damageArray)
+        const result =  countSpringArrangements(newConditionReport, damageArray)
+        if (result) totalCount += result
     }
 } else {
     console.log({damageArray})
     // if conditionReport has no unknowns then check to see if it matches the grouping
     const reportGroupings = conditionReport.split('.').filter(item=>!!item)
     console.log({reportGroupings})
-    let numWays = 0
     for (let i = 0; i < damageArray.length; i++){
-        if (reportGroupings[i].length !== damageArray[i]){
+        if (reportGroupings.length !== damageArray.length || reportGroupings[i].length !== damageArray[i]){
             console.log('grouping doesnot match, should return 0')
-            break
+            return 0
         }
     }
     console.log('group matches, return a 1')
-    returnValue = 1
+    return 1
 }
-return returnValue
+return totalCount
 
 }
 
 // put inputs from text file into an array
 const puzzleData = fs.readFileSync('./input.txt', 'utf8').split('\n').map(line => line.split(' '))
 console.log({puzzleData})
+let totalCount = 0
 puzzleData.forEach(line =>{
     const [conditionReport, damageGrouping] = line
     const damageArray = damageGrouping.split(',').map(string => Number(string))
-    console.log(countSpringArrangements(conditionReport, damageArray))
+    totalCount += (countSpringArrangements(conditionReport, damageArray))
 })
 
+console.log({totalCount})
