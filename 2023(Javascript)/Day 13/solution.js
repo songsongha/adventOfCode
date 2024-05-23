30a0a635b675df11556b7e019b539133802fe413
Song <songsongha@gmail.com>
2024-05-23 10:35:12 -0600

    Day 13 Add Part 2 Solution

--- 2023(Javascript)/Day 13/solution.js
+++ 2023(Javascript)/Day 13/solution.js
@@ -1,5 +1,5 @@
 // Problem 1: Find where the mirrors are, add up the number of columns to the left of each vertical line of reflection and add 100xnumber of rows abovev each horizontal line fo relction.
-// Problem 2: 
+// Problem 2: Find the smudge that creates the new reflection line, what is the new reflection line sum?
 
 const fs = require('fs')
 const transpose = (arr) => {
@@ -17,6 +17,18 @@ const checkMirrorLine = (index, pattern) => {
         }
     }
     return true
+}
+
+const checkDifference = (index, pattern) => {
+    let difference = 0
+    for (let i = 0; (index + i + 1) < pattern[0].length && index - i >= 0; i++){
+        const leftIndex = index - i
+        const rightIndex = index + i + 1
+        for (let rowIndex = 0; rowIndex < pattern.length; rowIndex++){
+            if (pattern[rowIndex][leftIndex] !== pattern[rowIndex][rightIndex]) difference += 1
+        }
+    }
+    return difference
 }
 
 const findVertMirrorIndex = (pattern) => {
@@ -44,6 +56,15 @@ const findVertMirrorIndex = (pattern) => {
     return mirrorIndex
 }
 
+const findSmudgeVertMirrorIndex = (pattern) => {
+    for (let colIndex = 0; colIndex < pattern[0].length -1; colIndex++){
+            if (checkDifference(colIndex,pattern) === 1){
+                return colIndex
+        }
+    }
+    return null
+}
+
 // put inputs from text file into an array
 const puzzleData = fs.readFileSync('./input.txt', 'utf8').split('\n\n').map(line => line.split('\n')).filter(item=>!!item)
 console.log({puzzleData})
@@ -56,6 +77,25 @@ puzzleData.forEach(pattern => {
         const transposedPattern = transpose(pattern)
         // redo the action
         const horizMirrorIndex = findVertMirrorIndex(transposedPattern)
+        // add amount to total
+        if(horizMirrorIndex === null) throw new Error('both hoeizontal and vertical are null!!')
+        sum += (horizMirrorIndex + 1) * 100
+    } else {
+        // add amount to count
+        sum += vertMirrorIndex + 1
+    }
+        console.log({sum})
+})
+
+// Part 2:
+puzzleData.forEach(pattern => {
+    // check for vertical mirror 
+    const vertMirrorIndex = findSmudgeVertMirrorIndex(pattern)
+    if (vertMirrorIndex === null) {
+        // transpose the pattern
+        const transposedPattern = transpose(pattern)
+        // redo the action
+        const horizMirrorIndex = findSmudgeVertMirrorIndex(transposedPattern)
         // add amount to total
         if(horizMirrorIndex === null) throw new Error('both hoeizontal and vertical are null!!')
         sum += (horizMirrorIndex + 1) * 100
