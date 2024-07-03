488f5de531ea2c05d9ff49e6ca511971747d1538
Song <songsongha@gmail.com>
2024-07-03 11:20:57 -0600

    Fix str to num issue, check for null values

--- 2023(Javascript)/Day 17/solution.js
+++ 2023(Javascript)/Day 17/solution.js
@@ -4,49 +4,65 @@
 const fs = require('fs')
 
 // put inputs from text file into an array
-const puzzleData = fs.readFileSync('./input.txt', 'utf8').split('\n')
+const puzzleData = fs.readFileSync('./input.txt', 'utf8').split('\n').map(str => str.split('').map(Number))
 console.log({puzzleData})
 
 function getMinOfMap(map) {
     const min = Array.from(map).reduce((min,curr)=> {
+        const [minX,minY] = min[0].split(',')
         const [x,y] = curr[0].split(',')
-        curr[1] < min[1] && puzzleData[x][y] !== null ? curr : min
+        if (puzzleData[minX][minY] === null) return curr
+        return curr[1] < min[1] && puzzleData[x][y] !== null ? curr : min
     })
     // returns location of minimum map that hasn't been visited
+    console.log({min})
+    const [minX,minY] = min[0].split(',')
+    console.log('puzzleData[minX][minY]', puzzleData[minX][minY])
+    if (puzzleData[minX][minY] === null) return null
     return min[0];
   }
 // starting in lefthand corner
 const queue = [[0,0]]
 const heatMap = new Map()
-const initialHeatLoss = puzzleData[0][0]
-console.log ({initialHeatLoss})
-heatMap.set('0,0', 0) 
+
+heatMap.set('0,0', puzzleData[0][0]) 
 while (queue.length > 0) {
-    const location = queue.shift()
+    const location = queue.shift().map(str => Number(str))
     const [row, col] = location
-    if (row === puzzleData.length -1 && col === puzzleData[0].length -1) break
-    puzzleData[row][col] = null
+    console.log({row})
+    console.log({col})
+    const minCumulativeHeatLossAtLocation = heatMap.get(`${row},${col}`)
     // check adjacent squares 
     if (row + 1 < puzzleData.length){
-        const below = puzzleData[row + 1][col]
-        heatMap.set(`${row + 1},${col}`, below)
+        const location = `${row + 1},${col}`
+        const value = puzzleData[row + 1][col] + minCumulativeHeatLossAtLocation
+        const prevValue = heatMap.get(location)
+        if (!prevValue || value < prevValue) heatMap.set(location, value)
     }
     if (row - 1 >= 0){
-        const above = puzzleData[row - 1][col]
-        heatMap.set(`${row - 1},${col}`, above)
+        const location = `${row - 1},${col}`
+        const value = puzzleData[row - 1][col] + minCumulativeHeatLossAtLocation
+        const prevValue = heatMap.get(location)
+        if (!prevValue || value < prevValue) heatMap.set(location, value)
     }
     if (col + 1 < puzzleData[0].length){
-        const right = puzzleData[row][col+1]
-        heatMap.set(`${row},${col+1}`, right)
+        const location = `${row},${col+1}`
+        const value = puzzleData[row][col+1] + minCumulativeHeatLossAtLocation
+        const prevValue = heatMap.get(location)
+        if (!prevValue || value < prevValue) heatMap.set(location, value)
     }
     if (col - 1 >= 0){
-        const left = puzzleData[row][col-1]
-        heatMap.set(`${row},${col-1}}`, left)
+        const location = `${row},${col-1}`
+        const value = puzzleData[row][col-1] + minCumulativeHeatLossAtLocation
+        const prevValue = heatMap.get(location)
+        if (!prevValue || value < prevValue) heatMap.set(location, value)
     }
-console.log({initialHeatLoss})
-const nextNode = getMinOfMap(heatMap).split(',')
-queue.push(nextNode)
+    puzzleData[row][col] = null
+    const nextNode = getMinOfMap(heatMap)
+    if(nextNode === null) break
+    queue.push(nextNode.split(','))
+
     
 }
 
-clumsyCrucible(0,0)
+console.log(heatMap.get(`${puzzleData.length -1},${puzzleData[0].length-1}`))
