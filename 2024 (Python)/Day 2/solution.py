959c45eef68344d1fb34486326b6a931442cd8eb
Song <songsongha@gmail.com>
2024-12-04 23:42:56 -0700

    WIP Day 2 Part 1

---
+++ 2024 (Python)/Day 2/solution.py
@@ -0,0 +1,30 @@
+
+# Part 1: Find how many reports are safe
+report_array = []
+
+with open("inputs.txt", "r") as file:
+    for line in file:
+        report_array.append(list(map(int, line.split(' '))))
+
+def isSafe(report):
+    is_increasing = None
+    for i in range(len(report)-2):
+        difference = report[i+1] - report[i]
+        if difference == 0:
+            return False
+        if i == 0:
+            is_increasing = (False, True) [difference > 0]
+        else:
+            if (is_increasing is True and difference < 0) or (is_increasing is False and difference > 0):
+                return False
+        if abs(difference) > 3:
+            return False
+    return True
+
+safe_count = 0
+for report in report_array:
+    if isSafe (report) is True:
+        safe_count +=1
+    
+print(safe_count)        
+
