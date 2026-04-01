2118dd0c04c30f808c0c18131e4ef093d8af8699
Song <songsongha@gmail.com>
2026-04-01 15:45:41 -0600

    WIP last year puzzle

--- 2024 (Python)/Day 3/solution.py
+++ 2024 (Python)/Day 3/solution.py
@@ -3,18 +3,30 @@
 
 import re
 
-input = ''
+input_string = ''
 
 with open("inputs.txt", "r") as file:
-    input = file.read()
+    input_string = file.read()
 
-regex = r"mul\((\d+),(\d+)\)"
+def getCalcs(input):
+    regex = r"mul\((\d+),(\d+)\)"
 
-calculations = re.findall(regex, input)
+    calculations = re.findall(regex, input)
 
-total_sum = 0
-for match in calculations:
-    num1, num2 = map(int, match)
-    total_sum += num1 * num2
+    total_sum = 0
+    for match in calculations:
+        num1, num2 = map(int, match)
+        total_sum += num1 * num2
 
-print(total_sum)
+    return total_sum
+
+print('Part1', getCalcs(input_string))
+
+regex2 = r"don't\(\).*?do\(\)"
+result = re.sub(regex2, "", input_string)
+result = re.sub(regex2, "", result)
+result = re.sub(regex2, "", result)
+result = re.sub(regex2, "", result)
+
+print(result)
+print('Part2', getCalcs(result))
