caf8ac76b3348f202efc684607dfcd4034c25691
Song <songsongha@gmail.com>
2026-04-02 14:05:08 -0600

    Day 2 part 2

--- 2025/Day 2/solutions.py
+++ 2025/Day 2/solutions.py
@@ -12,10 +12,20 @@ def check_for_match(id:int):
         if id_str[i] != id_str[halfway_index+i]:
             return False
     return True
+
+def check_for_match_part2(id:int):
+    id_str = str(id)
+    length = len(id_str)
+    for chunk_size in range(1, length):
+        if length % chunk_size == 0:  # must divide evenly
+            chunk = id_str[:chunk_size]
+            if chunk * (length // chunk_size) == id_str:
+                return True
+    return False
     
-        
-print (id_data)
-# brute force
+
+# Part 1: Sum up all ids with double repeating values
+# Part 2: Sum up all ids with any repeating values
 total = 0
 for id_range in id_data:
     lower, upper = map(int, id_range.split('-'))
@@ -26,14 +36,15 @@ for id_range in id_data:
         length = len(str(id))
         halfway_index = length//2
         print(halfway_index)
-        if length % 2 != 0 or length == 1:
-            # no way to have matching double number
-            # round to nearest double number with even number of places
-            half_value = 10 ** halfway_index
-            id = int(str(half_value) + str(half_value))
-            continue
-        # check for matching values
-        if check_for_match(id):
+        # only needed for part 1
+        # if length % 2 != 0 or length == 1:
+        #     # no way to have matching double number
+        #     # round to nearest double number with even number of places
+        #     half_value = 10 ** halfway_index
+        #     id = int(str(half_value) + str(half_value))
+        #     continue
+        # # check for matching values,
+        if check_for_match_part2(id):
             total += id
-        id = id+1 # change this to next duplicate number for efficiency
+        id = id+1 
 print(total)
